6fd85a386fb53f9894ff67ee8ec3350c276be6cf
Safaet Hosan <safaet@Safaet.local>
2023-01-20 17:31:17 -0500

    Added RollerCoasterLogic

--- RollarCoaster.py
+++ RollarCoaster.py
@@ -1,2 +1,25 @@
+print("-----< WELCOME TO THE ROLER COASTER >-----")
 
+height = int(input("What is your height in cm? "))
 
+if height >= 120:
+    print("You can ride!")
+    age = int(input("What is your age? "))
+
+    if age < 12:
+        bill = 7
+        print("Child tickets are $5")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7")
+    else:
+        bill = 12
+        print("Adult tickets are $12")
+
+    wants_photo = input("Do you want a photo ? Y/N ")
+    if wants_photo == "Y":
+        bill = bill+3
+    print(f"Your bill is ${bill}")
+
+else:
+    print("You can not ride")
