9e3ea839ad46229560f242635138504ca4fd0b06
Safaet Hosan <safaet@Safaet.local>
2023-01-20 16:56:02 -0500

    removing space

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 
 # This is how its working out whether if a particular year is a leap year.
-#
 # on every year that is evenly divisible by 4
-#
 # **except** every year that is evenly divisible by 100
-#
 # **unless** the year is also evenly divisible by 400
 
 print("---< Welcome to Leap Year Calculator >---")
