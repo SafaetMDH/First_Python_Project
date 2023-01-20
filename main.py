b333dfdf1a8428f9d7302848d2972ccef2c9827c
Safaet Hosan <safaet@Safaet.local>
2023-01-20 16:53:59 -0500

    added how it calculate

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+
+# This is how its working out whether if a particular year is a leap year.
+#
+# on every year that is evenly divisible by 4
+#
+# **except** every year that is evenly divisible by 100
+#
+# **unless** the year is also evenly divisible by 400
+
 print("---< Welcome to Leap Year Calculator >---")
 
 year = int(input("Which year do you want to check? "))
