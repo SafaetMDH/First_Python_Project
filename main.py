2b12af1415fc8dd1aab0f8cf137e3c3522d662ed
Safaet Hosan <safaet@Safaet.local>
2023-01-20 16:51:02 -0500

    Modifying

---
+++ main.py
@@ -0,0 +1,12 @@
+print("---< Welcome to Leap Year Calculator >---")
+
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    print("Leap year.")
+elif year % 100 == 0:
+    print("Leap year.")
+elif year % 400 == 0:
+    print("Leap year.")
+else:
+    print("Not leap year.")
